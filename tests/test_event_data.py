b835d5501b7ecb6b5250d6e8b334a8f37d67c34e
Buffy <buffy@freebuff.local>
2026-08-07 20:45:50 +0700

    data(arc2): wiring event intro 3 faksi quest Arc 2

--- tests/test_event_data.py
+++ tests/test_event_data.py
@@ -48,6 +48,9 @@ EXPECTED_EVENTS = {
     "quest208_done",
     "macan_baja_recruit",
     "memory_sekte_intrik",
+    "fquest_gilda_kontrak_intro",
+    "fquest_orde_arsip_intro",
+    "fquest_pemberontak_obat_intro",
 }
 REQUIRED_KEYS = {"id", "trigger", "actions", "once"}
 
